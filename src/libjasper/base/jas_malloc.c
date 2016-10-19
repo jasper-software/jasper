0c59a93f1b6433f259321939ee5f49eb45aa1e22
Michael Adams <mdadams@ece.uvic.ca>
2016-10-19 15:29:20 -0700

    Added some extra debugging log messages for memory allocation/deallocation.

--- src/libjasper/base/jas_malloc.c
+++ src/libjasper/base/jas_malloc.c
@@ -101,6 +101,7 @@
 void *jas_malloc(size_t size)
 {
 	void *result;
+	JAS_DBGLOG(101, ("jas_malloc called with %zu\n", size));
 	result = malloc(size);
 	JAS_DBGLOG(100, ("jas_malloc(%zu) -> %p\n", size, result));
 	return result;
@@ -108,7 +109,9 @@ void *jas_malloc(size_t size)
 
 void *jas_realloc(void *ptr, size_t size)
 {
-	void *result = realloc(ptr, size);
+	void *result;
+	JAS_DBGLOG(101, ("jas_realloc called with %x,%zu\n", ptr, size));
+	result = realloc(ptr, size);
 	JAS_DBGLOG(100, ("jas_realloc(%p, %zu) -> %p\n", ptr, size, result));
 	return result;
 }
