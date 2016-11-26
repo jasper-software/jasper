baefddd659c62811b6092cbb806865c7bdb8c291
Michael Adams <mdadams@ece.uvic.ca>
2016-11-26 01:29:22 -0800

    Some Appveyor changes

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -5,8 +5,9 @@ set source_dir=%APPVEYOR_BUILD_FOLDER%
 
 rem set build_dir=c:\tmp\jasper\build
 set build_dir=%source_dir%
+set allow_in_source_build=-DALLOW_IN_SOURCE_BUILD=true
 
-cmake -G "Visual Studio 12 2013 Win64" -H%source_dir% -B%build_dir% -DCMAKE_INSTALL_PREFIX=%install_prefix% || exit /B 1
+cmake -G "Visual Studio 12 2013 Win64" -H%source_dir% -B%build_dir% %allow_in_source_build% -DCMAKE_INSTALL_PREFIX=%install_prefix% || exit /B 1
 
 msbuild %build_dir%\INSTALL.vcxproj || exit /B 1
 
