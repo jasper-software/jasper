85a71b69981c8fb522b72c5b1eac0373e3aae3e9
Michael Adams <mdadams@ece.uvic.ca>
2016-11-26 01:16:20 -0800

    Some Appveyor changes

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -1,5 +1,10 @@
-cmake -G "Visual Studio 12 2013 Win64" -DCMAKE_INSTALL_PREFIX=c:\tmp\jasper -DALLOW_IN_SOURCE_BUILD=true . || exit /B 1
+echo on
+
+set install_prefix=c:\tmp\jasper\install
+set build_dir=c:\tmp\jasper\build
+
+cmake -G "Visual Studio 12 2013 Win64" -DCMAKE_INSTALL_PREFIX=%install_prefix% -DALLOW_IN_SOURCE_BUILD=true . || exit /B 1
+
+msbuild INSTALL.vcxproj || exit /B 1
 
 rem msbuild INSTALL.vcxproj /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit /B 1
-
-msbuild INSTALL.vcxproj /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || exit /B 1
