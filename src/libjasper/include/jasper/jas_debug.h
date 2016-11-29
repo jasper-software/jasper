fbbc1de5aab0b96976f2df6a26e2d480047a49f6
Michael Adams <mdadams@ece.uvic.ca>
2016-11-29 09:05:29 -0800

    Export more functions to DLL.

--- src/libjasper/include/jasper/jas_debug.h
+++ src/libjasper/include/jasper/jas_debug.h
@@ -104,7 +104,7 @@ JAS_DLLEXPORT int jas_getdbglevel(void);
 JAS_DLLEXPORT int jas_setdbglevel(int dbglevel);
 
 /* Perform formatted output to standard error. */
-int jas_eprintf(const char *fmt, ...);
+JAS_DLLEXPORT int jas_eprintf(const char *fmt, ...);
 
 /* Dump memory to a stream. */
 int jas_memdump(FILE *out, void *data, size_t len);
