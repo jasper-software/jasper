86d93442fbbaeaab1962d311f2c35f88483602c7
Michael Adams <mdadams@ece.uvic.ca>
2016-11-29 09:19:28 -0800

    Change build script to build both static and shared libraries under Windows.

--- build/appveyor/build.bat
+++ build/appveyor/build.bat
@@ -1,21 +1,38 @@
 echo on
 
-set install_prefix=c:\tmp\jasper\install
 set source_dir=%APPVEYOR_BUILD_FOLDER%
 
-rem THE FOLLOWING IS FOR AN IN-SOURCE BUILD:
-rem set build_dir=%source_dir%
-rem set allow_in_source_build=-DALLOW_IN_SOURCE_BUILD=true
+set build_dir_shared=c:\tmp\jasper\build\shared
+set build_dir_static=c:\tmp\jasper\build\static
+set install_dir_shared=c:\tmp\jasper\install\shared
+set install_dir_static=c:\tmp\jasper\install\static
 
-rem THE FOLLOWING IS FOR AN OUT-OF-SOURCE BUILD:
-set build_dir=c:\tmp\jasper\build
+@echo ############################################################
+@echo Static Library
+@echo ############################################################
 
 cmake -G "Visual Studio 12 2013 Win64" ^
-  -H%source_dir% -B%build_dir% %allow_in_source_build% ^
-  -DCMAKE_INSTALL_PREFIX=%install_prefix% || exit /B 1
+  -H%source_dir% -B%build_dir_static% %allow_in_source_build% ^
+  -DCMAKE_INSTALL_PREFIX=%install_dir_static% ^
+  -DJAS_ENABLE_SHARED=false || exit /B 1
 
-msbuild %build_dir%\INSTALL.vcxproj || exit /B 1
+msbuild %build_dir_static%\INSTALL.vcxproj || exit /B 1
 
-dir %install_prefix%\include\jasper || exit /B 1
-dir %install_prefix%\bin || exit /B 1
-dir %install_prefix%\lib || exit /B 1
+dir %install_dir_static%\include\jasper || exit /B 1
+dir %install_dir_static%\bin || exit /B 1
+dir %install_dir_static%\lib || exit /B 1
+
+@echo ############################################################
+@echo Shared Library
+@echo ############################################################
+
+cmake -G "Visual Studio 12 2013 Win64" ^
+  -H%source_dir% -B%build_dir_shared% %allow_in_source_build% ^
+  -DCMAKE_INSTALL_PREFIX=%install_dir_shared% ^
+  -DJAS_ENABLE_SHARED=true || exit /B 1
+
+msbuild %build_dir_shared%\INSTALL.vcxproj || exit /B 1
+
+dir %install_dir_shared%\include\jasper || exit /B 1
+dir %install_dir_shared%\bin || exit /B 1
+dir %install_dir_shared%\lib || exit /B 1
