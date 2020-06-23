a6c219d4c3d90f2a870c7b1bd037ddbd085df75c
Max Kellermann <max.kellermann@gmail.com>
2020-06-23 12:18:46 +0200

    jas_seq: remove unnecessary NULL checks from jas_matrix_destroy()
    Calling `jas_free(NULL)` is legal.  And it's pointless to clear the
    pointers in an object that will be freed - the compiler is allowed to
    optimize that code away.

--- src/libjasper/base/jas_seq.c
+++ src/libjasper/base/jas_seq.c
@@ -166,13 +166,9 @@ void jas_matrix_destroy(jas_matrix_t *matrix)
 {
 	if (matrix->data_) {
 		assert(!(matrix->flags_ & JAS_MATRIX_REF));
-		jas_free(matrix->data_);
-		matrix->data_ = 0;
 	}
-	if (matrix->rows_) {
-		jas_free(matrix->rows_);
-		matrix->rows_ = 0;
-	}
+	jas_free(matrix->data_);
+	jas_free(matrix->rows_);
 	jas_free(matrix);
 }
 
