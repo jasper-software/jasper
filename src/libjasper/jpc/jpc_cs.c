60c3c0602070b36cd6fc3c5f6a6d5aa2ae860a14
Max Kellermann <max.kellermann@gmail.com>
2020-06-24 22:31:55 +0200

    jpc_cs: add missing jpc_putuint8() error check

--- src/libjasper/jpc/jpc_cs.c
+++ src/libjasper/jpc/jpc_cs.c
@@ -1097,7 +1097,9 @@ static int jpc_qcx_putcompparms(jpc_qcxcp_t *compparms, jpc_cstate_t *cstate,
 	/* Eliminate compiler warning about unused variables. */
 	cstate = 0;
 
-	jpc_putuint8(out, ((compparms->numguard & 7) << 5) | compparms->qntsty);
+	if (jpc_putuint8(out, ((compparms->numguard & 7) << 5) | compparms->qntsty)) {
+		return -1;
+	}
 	for (i = 0; i < compparms->numstepsizes; ++i) {
 		if (compparms->qntsty == JPC_QCX_NOQNT) {
 			if (jpc_putuint8(out, JPC_QCX_GETEXPN(
