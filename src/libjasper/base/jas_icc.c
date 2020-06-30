e3f817d75a163a94ae48978bf2751befaa580cf3
Max Kellermann <max.kellermann@gmail.com>
2020-06-30 19:19:01 +0200

    jas_icc: remove unnecessary error cleanup code
    When the `error` label is reached, `tmpattrval==NULL` in all cases.

--- src/libjasper/base/jas_icc.c
+++ src/libjasper/base/jas_icc.c
@@ -758,18 +758,17 @@ static int jas_iccattrtab_add(jas_iccattrtab_t *attrtab, int i,
 	int n;
 	jas_iccattr_t *attr;
 	jas_iccattrval_t *tmpattrval;
-	tmpattrval = 0;
 	if (i < 0) {
 		i = attrtab->numattrs;
 	}
 	assert(i >= 0 && i <= attrtab->numattrs);
 	if (attrtab->numattrs >= attrtab->maxattrs) {
 		if (jas_iccattrtab_resize(attrtab, attrtab->numattrs + 32)) {
-			goto error;
+			return -1;
 		}
 	}
 	if (!(tmpattrval = jas_iccattrval_clone(val)))
-		goto error;
+		return -1;
 	n = attrtab->numattrs - i;
 	if (n > 0)
 		memmove(&attrtab->attrs[i + 1], &attrtab->attrs[i],
@@ -779,10 +778,6 @@ static int jas_iccattrtab_add(jas_iccattrtab_t *attrtab, int i,
 	attr->val = tmpattrval;
 	++attrtab->numattrs;
 	return 0;
-error:
-	if (tmpattrval)
-		jas_iccattrval_destroy(tmpattrval);
-	return -1;
 }
 
 static int jas_iccattrtab_replace(jas_iccattrtab_t *attrtab, int i,
