3ba00895d457f749bd72a9f77bad124ffd1a0341
Max Kellermann <max.kellermann@gmail.com>
2020-08-07 22:02:01 +0200

    jpc_dec: convert abort() to assert()
    Check for code bugs only in debug builds.

--- src/libjasper/jpc/jpc_dec.c
+++ src/libjasper/jpc/jpc_dec.c
@@ -2299,11 +2299,10 @@ static int jpc_streamlist_insert(jpc_streamlist_t *streamlist, int streamno,
 
 static jas_stream_t *jpc_streamlist_remove(jpc_streamlist_t *streamlist, int streamno)
 {
+	assert(streamno < streamlist->numstreams);
+
 	jas_stream_t *stream;
 	int i;
-	if (streamno >= streamlist->numstreams) {
-		abort();
-	}
 	stream = streamlist->streams[streamno];
 	for (i = streamno + 1; i < streamlist->numstreams; ++i) {
 		streamlist->streams[i - 1] = streamlist->streams[i];
