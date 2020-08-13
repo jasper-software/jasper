59ac1ea3662111ad238e02c0b0bf14269403a0bc
Max Kellermann <max.kellermann@gmail.com>
2020-08-13 16:29:46 +0200

    jas_stream: remove unused `openflags` variable from jas_stream_freopen()

--- src/libjasper/base/jas_stream.c
+++ src/libjasper/base/jas_stream.c
@@ -441,7 +441,6 @@ jas_stream_t *jas_stream_fopen(const char *filename, const char *mode)
 jas_stream_t *jas_stream_freopen(const char *path, const char *mode, FILE *fp)
 {
 	jas_stream_t *stream;
-	int openflags;
 
 	JAS_DBGLOG(100, ("jas_stream_freopen(\"%s\", \"%s\", %p)\n", path, mode,
 	  fp));
@@ -456,27 +455,6 @@ jas_stream_t *jas_stream_freopen(const char *path, const char *mode, FILE *fp)
 
 	/* Parse the mode string. */
 	stream->openmode_ = jas_strtoopenmode(mode);
-
-	/* Determine the correct flags to use for opening the file. */
-	if ((stream->openmode_ & JAS_STREAM_READ) &&
-	  (stream->openmode_ & JAS_STREAM_WRITE)) {
-		openflags = O_RDWR;
-	} else if (stream->openmode_ & JAS_STREAM_READ) {
-		openflags = O_RDONLY;
-	} else if (stream->openmode_ & JAS_STREAM_WRITE) {
-		openflags = O_WRONLY;
-	} else {
-		openflags = 0;
-	}
-	if (stream->openmode_ & JAS_STREAM_APPEND) {
-		openflags |= O_APPEND;
-	}
-	if (stream->openmode_ & JAS_STREAM_BINARY) {
-		openflags |= O_BINARY;
-	}
-	if (stream->openmode_ & JAS_STREAM_CREATE) {
-		openflags |= O_CREAT | O_TRUNC;
-	}
 
 	stream->obj_ = JAS_CAST(void *, fp);
 
