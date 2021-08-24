c87f1c879bce270bc479f1b3bbc02f083427d459
Michael Adams <mdadams@ece.uvic.ca>
2021-08-24 08:47:29 -0700

    Cleaned up the top-level CMakeLists.txt file.

--- src/libjasper/include/jasper/jas_dll.h
+++ src/libjasper/include/jasper/jas_dll.h
@@ -65,33 +65,6 @@
 /* The configuration header file should be included first. */
 #include <jasper/jas_config.h>
 
-#if 0
-#if defined(JAS_DLL)
-	#if defined(_WIN32)
-		#if defined(JAS_BUILDING_DLL)
-			#define JAS_DLLEXPORT __declspec(dllexport)
-		#else
-			#define JAS_DLLEXPORT __declspec(dllimport)
-		#endif
-		#define JAS_DLLLOCAL
-	#elif defined(JAS_HAVE_VISIBILITY)
-		#if defined(JAS_BUILDING_DLL)
-			#define JAS_DLLEXPORT __attribute__ ((visibility("default")))
-			#define JAS_DLLLOCAL __attribute__ ((visibility("hidden")))
-		#else
-			#define JAS_DLLEXPORT
-			#define JAS_DLLLOCAL
-		#endif
-	#else
-		#define JAS_DLLEXPORT
-		#define JAS_DLLLOCAL
-	#endif
-#else
-	#define JAS_DLLEXPORT
-	#define JAS_DLLLOCAL
-#endif
-#endif
-
 #include <jasper/jas_export_cmake.h>
 
 /* For backward compatibility only. */
