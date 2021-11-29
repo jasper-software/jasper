1b62bfdfb02b0f8a15f43a974693e7e60b91967e
Michael Adams <mdadams@ece.uvic.ca>
2021-11-29 15:13:22 -0800

    Added some missing type casts in the tag-value parser code. Marked some types of image-related files as binary in the .gitattributes file.

--- src/libjasper/base/jas_tvp.c
+++ src/libjasper/base/jas_tvp.c
@@ -127,7 +127,7 @@ int jas_tvparser_next(jas_tvparser_t *tvp)
 
 	/* Skip any leading whitespace. */
 	p = tvp->pos;
-	while (*p != '\0' && isspace(*p)) {
+	while (*p != '\0' && isspace(JAS_CAST(unsigned char, *p))) {
 		++p;
 	}
 
@@ -139,7 +139,7 @@ int jas_tvparser_next(jas_tvparser_t *tvp)
 	}
 
 	/* Does the tag name begin with a valid character? */
-	if (!JAS_TVP_ISTAG(*p)) {
+	if (!JAS_TVP_ISTAG(JAS_CAST(unsigned char, *p))) {
 		return -1;
 	}
 
@@ -147,7 +147,7 @@ int jas_tvparser_next(jas_tvparser_t *tvp)
 	tag = p;
 
 	/* Find the end of the tag name. */
-	while (*p != '\0' && JAS_TVP_ISTAG(*p)) {
+	while (*p != '\0' && JAS_TVP_ISTAG(JAS_CAST(unsigned char, *p))) {
 		++p;
 	}
 
@@ -162,7 +162,7 @@ int jas_tvparser_next(jas_tvparser_t *tvp)
 
 	/* Is a value field not present? */
 	if (*p != '=') {
-		if (*p != '\0' && !isspace(*p)) {
+		if (*p != '\0' && !isspace(JAS_CAST(unsigned char, *p))) {
 			return -1;
 		}
 		*p++ = '\0';
@@ -175,7 +175,7 @@ int jas_tvparser_next(jas_tvparser_t *tvp)
 	*p++ = '\0';
 
 	val = p;
-	while (*p != '\0' && !isspace(*p)) {
+	while (*p != '\0' && !isspace(JAS_CAST(unsigned char, *p))) {
 		++p;
 	}
 
