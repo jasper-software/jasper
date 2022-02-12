2c3517f528f6ffd5f8ce4af604c8cdc558d0bf1d
Michael Adams <mdadams@ece.uvic.ca>
2022-02-12 05:44:51 -0800

    Fixes #317.
    Changed two occurrences of WIN32 to _WIN32 in the I/O stream code.

--- src/libjasper/base/jas_stream.c
+++ src/libjasper/base/jas_stream.c
@@ -102,7 +102,7 @@
 #include <unistd.h>
 #endif
 
-#if defined(WIN32) || defined(JAS_HAVE_IO_H)
+#if defined(_WIN32) || defined(JAS_HAVE_IO_H)
 #include <io.h>
 #endif
 
@@ -533,7 +533,7 @@ jas_stream_t *jas_stream_fdopen(int fd, const char *mode)
 	/* Parse the mode string. */
 	stream->openmode_ = jas_strtoopenmode(mode);
 
-#if defined(WIN32) || defined(JAS_HAVE_IO_H)
+#if defined(_WIN32) || defined(JAS_HAVE_IO_H)
 	/* Argh!!!  Someone ought to banish text mode (i.e., O_TEXT) to the
 	  greatest depths of purgatory! */
 	/* Ensure that the file descriptor is in binary mode, if the caller
