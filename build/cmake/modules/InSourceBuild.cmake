a798ba0ebadc103d5c3bb525bd2b7d0059c98be7
Michael Adams <mdadams@ece.uvic.ca>
2023-11-27 17:43:39 -0800

    Fixes #364.
    Made some changes to the InSourceBuild CMake module in an attempt
    to prevent CMake-challenged individuals from trashing the source
    tree when performing a build.

--- build/cmake/modules/InSourceBuild.cmake
+++ build/cmake/modules/InSourceBuild.cmake
@@ -1,15 +1,31 @@
 option(ALLOW_IN_SOURCE_BUILD "Allow an in-source build" OFF)
 
 function(prevent_in_source_build)
-	get_filename_component(source_dir "${CMAKE_SOURCE_DIR}" REALPATH)
-	get_filename_component(binary_dir "${CMAKE_BINARY_DIR}" REALPATH)
-	if(source_dir STREQUAL binary_dir)
+
+	# Determine if an in-source build is in progress.
+	file(REAL_PATH "${CMAKE_SOURCE_DIR}" source_dir)
+	file(REAL_PATH "${CMAKE_BINARY_DIR}" binary_dir)
+	cmake_path(IS_PREFIX source_dir "${binary_dir}" result)
+	cmake_path(GET binary_dir FILENAME binary_dir_base)
+
+	# If an in-source build is in progress, and the build directory is not
+	# chosen in a very specific way, then stop the build.
+	if(result AND (NOT (binary_dir_base MATCHES "tmp*")))
 		message(FATAL_ERROR
-			"The use of an in-source build is not recommended.  "
-			"For this reason, the use of in-source build is disabled by default.  "
-			"If you want to override this default behavior, add the -DALLOW_IN_SOURCE_BUILD option to cmake."
+			"The use of an in-source build has been detected "
+			"(i.e., the binary directory specified to CMake is located "
+			"in or under the source directory).  "
+			"This can potentially trash the source tree.  "
+			"In fact, if you are seeing this message, you may have already "
+			"partially trashed the source tree.  "
+			"The use of an in-source build is not officially supported and "
+			"is therefore disallowed by default.  "
+			"If you like to live dangerously and would like to override "
+			"this default behavior, this can be accomplished via the "
+			"CMake option ALLOW_IN_SOURCE_BUILD."
 		)
 	endif()
+
 endfunction()
 
 if(NOT ALLOW_IN_SOURCE_BUILD)
